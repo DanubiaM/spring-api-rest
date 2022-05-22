6179ad2d0fccacdfebfc9ed873fe014a22c21e89
DanubiaM <dubamacedoo@gmail.com>
2022-05-22 19:47:52 -0400

    refactor: using @RestController

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,14 +5,16 @@ import br.com.alura.forum.modelo.Curso;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+
+@RestController
 public class TopicosController {
+
     @RequestMapping("/topicos")
-    @ResponseBody
     public List<Object> lista(){
 
         Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
