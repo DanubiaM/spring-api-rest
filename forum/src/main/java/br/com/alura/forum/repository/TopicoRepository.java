3e3dbd399a299a7df697f4d1994b0a4a280c8fea
DanubiaM <dubamacedoo@gmail.com>
2022-06-03 20:44:09 -0400

    doc: added comment

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-
+    /*Curso refere-se à outra entidade relacionada à classe topico,
+    * outra maneira de se referir a entidade curso e seu atributo nome seria: findByCurso_nome
+    * */
     List<Topico> findByCursoNome(String nomeCurso);
 
 }
