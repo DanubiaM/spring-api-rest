35d7d4a73503331979f6ade10069bcbfd1eda155
DanubiaM <dubamacedoo@gmail.com>
2023-01-09 22:25:30 -0400

    feat:  authentication token filter implemented

---
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -0,0 +1,39 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/*
+* OncePerRequestFilter é como se fosse uma gatway, a requisição  irá passar primeiro nela para ir para o controller.
+*
+* */
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recuperarToken(request);
+
+        System.out.println(token);
+        //Valida token
+        filterChain.doFilter(request, response);
+
+
+
+    }
+
+    //Function para recuperar e verificar se o token é valido, caso valido é retornado.
+    private String recuperarToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (token == null | token.isEmpty() | !token.startsWith("Bearer")){
+            return null;
+        }
+        return token.substring(7, token.length());
+
+    }
+}
