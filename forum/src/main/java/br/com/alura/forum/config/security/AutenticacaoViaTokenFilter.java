da2495bf747d1b2a3af73636bc6f6495ed5c5627
DanubiaM <dubamacedoo@gmail.com>
2023-01-13 22:08:01 -0400

    feat: method to authenticate client

--- forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -27,6 +27,17 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
     }
 
+    private void autenticarCliente(String token) {
+        Long idUsuario = tokenService.getIdUsuario(token);
+
+        Usuario  usuario = usuarioRepository.findById(idUsuario).get();
+
+        UsernamePasswordAuthenticationToken  authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    }
+
     //Function para recuperar e verificar se o token é valido, caso valido é retornado.
     private String recuperarToken(HttpServletRequest request) {
         String token = request.getHeader("Authorization");
